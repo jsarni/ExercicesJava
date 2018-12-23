67188be9d73fa9afda76155338a61b69484e3f67
jsarni <juba.sarni@gmail.com>
2018-12-23 13:37:44 +0100

    part 1 and 2

--- Serie1/Exercise4/Exercise4/src/exo4/Exercise4.java
+++ Serie1/Exercise4/Exercise4/src/exo4/Exercise4.java
@@ -1,4 +1,31 @@
 package exo4;
 
 public class Exercise4 {
+    public static void main(String[] args){
+        String theAlphabet = args[0];
+        String theString = args[1];
+        if(!isCorrect(theAlphabet, theString)){
+            System.out.println("Error ! a letter is used in your string is unknown in the alphabet");
+        } else {
+            System.out.println("OK !");
+        }
+        rotateToLeft(3,theString);
+
+    }
+
+    private static boolean isCorrect(String alphabet, String stringToTest){
+        for(int i = 0; i < stringToTest.length(); i++){
+            if(alphabet.indexOf(i) == -1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void rotateToLeft(int nbChar, String theString){
+        for(int i = 0; i < nbChar; i++){
+            theString = (theString.substring(1)).concat(theString.substring(0,1));
+        }
+        System.out.println(theString);
+    }
 }
